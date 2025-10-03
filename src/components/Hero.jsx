12365717d5a88b2c99c9f17307c455079b625f67
javid shabin <javid.main.act@gmail.com>
2025-10-03 15:35:50 +0530

    Create the header and make background image for the hero seciont

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Hero = () => {
   return (
-    <div>Hero nn</div>
+    <div style={{backgroundImage: "url('/images/hero.png')", backgroundSize: "cover", backgroundPosition: "center"}}>
+        <div className='flex flex-col items-center justify-center h-screen'>
+            <h1 className='text-4xl font-bold'>Welcome to the Home Page</h1>
+            <p className='text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+        </div>
+    </div>
   )
 }
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Header } from '../components/Header'
 
 export const MainLayout = () => {
   return (
     <div>
+      <Header />
       <Outlet />
     </div>
   )
