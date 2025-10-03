8fa5b0f6ef7f33ba2d7071b514df1888ac1f837b
javid shabin <javid.main.act@gmail.com>
2025-10-03 16:17:29 +0530

    Resolve the problem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,8 @@ import { Hero } from '../components/Hero'
 
 export const Home = () => {
   return (
-    <div>Hello home nn
-
-      <h1>
-        Welcome to the Home Page
-      </h1>
+    <div>
+      <Hero />
     </div>
     
   )
